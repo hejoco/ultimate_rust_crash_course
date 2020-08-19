c211c1c92d30cdf2e45b1c24e743d3df001fa55c
hejoco <hjcooper@gmail.com>
2020-08-19 18:54:49 -0500

    exercise A variables

--- exercise/variables/src/main.rs
+++ exercise/variables/src/main.rs
@@ -1,3 +1,8 @@
+const MISSILES: i32 = 8;
+const READY: i32 = 2;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut missiles, ready): (i32, i32) = (MISSILES, READY);
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    println!("{} missiles left.", missiles - ready);
 }
